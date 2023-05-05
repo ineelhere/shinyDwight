f525904515b97d6e2bc932bfcc16f89e16aed23d
ineelhere <>
2023-05-05 23:02:37 +0530

    wip

--- app/logic/dockerinfo.R
+++ app/logic/dockerinfo.R
@@ -1,4 +1,6 @@
 library(httr)
+library(plotly)
+library(ggplot2)
 
 get_docker_image_info <- function(image_name) {
 
@@ -26,4 +28,56 @@ get_docker_image_info <- function(image_name) {
 }
 
 
-get_docker_image_info("ubuntu")
+image_info <- get_docker_image_info("ubuntu")
+
+# Create a data frame for the plot
+plot_data <- data.frame(
+  Count = c(image_info$Pull_Count, image_info$Star_Count),
+  Type = c("Pulls", "Stars")
+)
+
+# Generate the plot
+ggplotly(ggplot(plot_data, aes(x = "", y = Count, fill = Type)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  theme_void() +
+  theme(legend.position = "bottom") +
+  labs(title = "Ubuntu Docker Image",
+       subtitle = "Pulls and Stars Count",
+       fill = NULL,
+       x = NULL,
+       y = NULL))
+
+ggplotly(gg) %>%
+  layout(title = list(text = "Ubuntu Docker Image"),
+         xaxis = list(showticklabels = FALSE),
+         yaxis = list(showticklabels = FALSE),
+         showlegend = TRUE)
+
+
+# Generate the plot
+ggplot(plot_data, aes(x = "", y = Count, fill = Type)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  theme_void() +
+  theme(legend.position = "bottom") +
+  labs(title = "Ubuntu Docker Image",
+       subtitle = "Pulls and Stars Count",
+       fill = NULL,
+       x = NULL,
+       y = NULL) +
+  geom_text(aes(label = Count), position = position_stack(vjust = 0.5))
+
+
+# Create the plot
+plot_ly(data = plot_data, labels = ~Type, values = ~Count, type = 'pie',
+        marker = list(colors = c("#F8766D", "#00BFC4")),
+        text = ~paste0(plot_data$Type, ": ", plot_data$Count),
+        textinfo = "text",
+        hoverinfo = "skip") %>%
+  layout(title = "Ubuntu Docker Image", showlegend = TRUE,
+         annotations = list(text = "Count", showarrow = FALSE,
+                            x = 0.5, y = 0.5, font = list(size = 16))) %>%
+  add_annotations(text = plot_data$Count,
+                  x = c(0.25, 0.75), y = c(0.8, 0.2),
+                  showarrow = FALSE, font = list(size = 16))
