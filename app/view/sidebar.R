9eb5ad1768e9c5cfa2c4265fc922c8015f6f3991
ineelhere <>
2023-05-10 15:16:19 +0530

    [MOD] replace imgs with gifs

--- app/view/sidebar.R
+++ app/view/sidebar.R
@@ -5,16 +5,16 @@ box::use(
 ui <- function(id) {
   ns <- NS(id)
   div(class = "nav-icons",
-      div(class = "nav", img(src = "static/michael.png", title = "Michael Scott")),
-      div(class = "nav", img(src = "static/dwight.png", title = "Dwight Schrute")),
-      div(class = "nav", img(src = "static/jim.png", title = "Jim Halpert")),
-      div(class = "nav", img(src = "static/pam.png", title = "Pam Beesly")),
-      div(class = "nav", img(src = "static/phyllis.png", title = "Phyllis Vance")),
-      div(class = "nav", img(src = "static/stanley.png", title = "Stanley Hudson")),
-      div(class = "nav", img(src = "static/kevin.png", title = "Kevin Malone")),
-      div(class = "nav", img(src = "static/oscar.png", title = "Oscar Martinez")),
-      div(class = "nav", img(src = "static/angela.png", title = "Angela Martin")),
-      div(class = "nav", img(src = "static/andy.png", title = "Andy"))
+      div(class = "nav", img(src = "https://media.giphy.com/media/cXblnKXr2BQOaYnTni/giphy.gif", title = "Michael Scott")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/UX6huwJfADuH9khCMl/giphy.gif", title = "Dwight Schrute")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/Yycc82XEuWDaLLi2GV/giphy.gif", title = "Jim Halpert")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/D8xNev92dfqdG9FPx4/giphy.gif", title = "Pam Beesly")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/6rvh1enyaBISOojJMa/giphy.gif", title = "Phyllis Vance")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/C1tWTNs9tf70qXTJ5c/giphy.gif", title = "Stanley Hudson")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/3pkFrdxslEAE5RhPuL/giphy.gif", title = "Kevin Malone")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/fP1cA793NCpWzlEE96/giphy.gif", title = "Oscar Martinez")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/A14XC5WFpDpHjyH9Pz/giphy.gif", title = "Angela Martin")),
+      div(class = "nav", img(src = "https://media.giphy.com/media/nrg0TI3u0BFw5NBDsQ/giphy.gif", title = "Andy"))
 
   )
 }
